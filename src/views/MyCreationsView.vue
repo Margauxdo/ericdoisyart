25f8aaf3e0184752e5ad50cbc028460813728c04
Margaux Doisy <margauxdoisy@gmail.com>
2025-02-02 20:10:41 +0100

    filter mycreationsview

--- src/views/MyCreationsView.vue
+++ src/views/MyCreationsView.vue
@@ -2,12 +2,14 @@
 import { ref } from "vue";
 import Neoexpressionisme from "@/components/Neoexpressionisme.vue";
 import Figuratif from "@/components/Figuratif.vue";
+import FiltreOeuvres from "@/components/Filtre.vue";
 
 const styleOptions = ref(["Figuratif", "Néo-expressionnisme"]);
 const supportOptions = ref(["Bois", "Carton", "Acier", "Bois de palette", "Toile", "Toile de jute"]);
 
 const selectedStyle = ref(null);
 const selectedSupport = ref(null);
+const showFilterMenu = ref(false);
 
 const updateFilter = (filter) => {
   selectedStyle.value = filter.style;
@@ -18,7 +20,9 @@ const updateFilter = (filter) => {
 <template>
   <div class="oeuvres">
     <h1>Mes créations</h1>
-    <FiltreOeuvres @updateFilter="updateFilter" />
+
+    <button @click="showFilterMenu = !showFilterMenu" class="filter-button">Filtrer</button>
+    <FiltreOeuvres v-if="showFilterMenu" @updateFilter="updateFilter" />
 
     <div v-if="selectedStyle === 'Néo-expressionnisme' || !selectedStyle" id="neo-expressionnisme" class="neo-expressionnisme">
       <Neoexpressionisme :selectedSupport="selectedSupport" />
@@ -35,5 +39,12 @@ const updateFilter = (filter) => {
   text-align: center;
   padding: 20px;
 }
+.filter-button {
+  margin: 10px;
+  padding: 10px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  cursor: pointer;
+}
 </style>
-
